ba39d4ef2547db8f7a35aa0571070e5abb6225e0
andron13 <andron13@gmail.com>
2023-07-03 13:57:12 +0200

    feat: add --detectOpenHandles work on 7, note done

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
+
 import { throttledGetDataFromApi } from './index';
 
-// mockReturnValue needs AxiosInstance
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com',
 });
@@ -9,18 +9,9 @@ console.log(axiosInstance);
 
 describe('throttledGetDataFromApi', () => {
   afterEach(() => jest.resetAllMocks());
-
   const baseUrl = 'https://jsonplaceholder.typicode.com';
   const postsEndpoint = 'posts';
-  const onePost = 'posts/5';
-  const post = {
-    userId: 1,
-    id: 5,
-    title: 'nesciunt quas odio',
-    body: '',
-  };
-  const response = { data: post };
-  console.log(response, onePost);
+
   test('should create instance with provided base url', async () => {
     const createSpy = jest
       .spyOn(axios, 'create')
@@ -29,11 +20,4 @@ describe('throttledGetDataFromApi', () => {
     await throttledGetDataFromApi(postsEndpoint);
     expect(createSpy).toHaveBeenCalledWith({ baseURL: baseUrl });
   });
-
-  // test('should perform request to correct provided url', async () => {
-  //   const getSpy = jest.spyOn(axios, 'get').mockResolvedValue(response);
-  //   await throttledGetDataFromApi(onePost);
-  //   jest.runAllTimers();
-  //   expect(getSpy).toBeCalledWith(onePost);
-  // });
 });
