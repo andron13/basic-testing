5baaee547d55edff9812266d434c42c27da0e78e
andron13 <andron13@gmail.com>
2024-02-04 14:01:07 +0100

    fix: minor changes

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,23 +1,56 @@
-import axios, { AxiosInstance } from 'axios';
-
+import axios from 'axios';
 import { throttledGetDataFromApi } from './index';
 
-const axiosInstance: AxiosInstance = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com',
-});
-console.log(axiosInstance);
+const API_ENDPOINT = 'posts';
+const API_BASE = 'https://jsonplaceholder.typicode.com';
+const MOCKED_DATA = { data: 'test' };
 
-describe('throttledGetDataFromApi', () => {
-  afterEach(() => jest.resetAllMocks());
-  const baseUrl = 'https://jsonplaceholder.typicode.com';
-  const postsEndpoint = 'posts';
+describe('API data retrieval', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
 
-  test('should create instance with provided base url', async () => {
-    const createSpy = jest
-      .spyOn(axios, 'create')
-      .mockReturnValue(axiosInstance);
+  afterAll(() => {
+    jest.useRealTimers();
+  });
 
-    await throttledGetDataFromApi(postsEndpoint);
-    expect(createSpy).toHaveBeenCalledWith({ baseURL: baseUrl });
+  test('checks if instance is created with correct base url', async () => {
+    const instanceCreationMock = jest.spyOn(axios, 'create');
+
+    await throttledGetDataFromApi(API_ENDPOINT);
+
+    expect(instanceCreationMock).toHaveBeenCalledWith({
+      baseURL: API_BASE,
+    });
+  });
+
+  test('validates if request is made to the correct url', async () => {
+    const axiosClient = axios.create({
+      baseURL: API_BASE,
+    });
+    const axiosGetMock = jest.spyOn(axiosClient, 'get');
+    const instanceCreationMock = jest.spyOn(axios, 'create');
+    instanceCreationMock.mockReturnValue(axiosClient);
+
+    await throttledGetDataFromApi(API_ENDPOINT);
+
+    expect(axiosGetMock).not.toHaveBeenCalled();
+
+    jest.runAllTimers();
+
+    expect(axiosGetMock).toHaveBeenCalledWith(API_ENDPOINT);
+  });
+
+  test('ensures response data is returned correctly', async () => {
+    const axiosClient = axios.create({
+      baseURL: API_BASE,
+    });
+    const axiosGetMock = jest.spyOn(axiosClient, 'get');
+    const instanceCreationMock = jest.spyOn(axios, 'create');
+    instanceCreationMock.mockReturnValue(axiosClient);
+    axiosGetMock.mockReturnValue(Promise.resolve(MOCKED_DATA));
+
+    expect(await throttledGetDataFromApi(API_ENDPOINT)).toBe(MOCKED_DATA.data);
+    expect(axiosGetMock).toHaveBeenCalled();
   });
 });
--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,17 +1,40 @@
 import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  test('should generate linked list from values 1', () => {
+  test('should generate linked list from set of values object snapshot', () => {
     const elements = [1, 2, 3];
     const result = generateLinkedList(elements);
 
     expect(result).toMatchSnapshot();
   });
 
-  test('should generate linked list from values 2', () => {
+  test('should generate linked list from different set of values object snapshot', () => {
     const elements = ['a', 'b', 'c'];
     const result = generateLinkedList(elements);
 
     expect(result).toMatchSnapshot();
+  });
+
+  test('should generate correct linked list from numeric values', () => {
+    expect(generateLinkedList([1])).toStrictEqual({
+      value: 1,
+      next: { value: null, next: null },
+    });
+
+    expect(generateLinkedList([1, 2])).toStrictEqual({
+      value: 1,
+      next: { value: 2, next: { value: null, next: null } },
+    });
+
+    expect(generateLinkedList([1, 2, 3])).toStrictEqual({
+      value: 1,
+      next: { value: 2, next: { value: 3, next: { value: null, next: null } } },
+    });
+  });
+
+  test('should generate correct linked list from numeric values snapshot', () => {
+    expect(generateLinkedList([1])).toMatchSnapshot();
+    expect(generateLinkedList([1, 2])).toMatchSnapshot();
+    expect(generateLinkedList([1, 2, 3])).toMatchSnapshot();
   });
 });
